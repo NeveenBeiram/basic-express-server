b3471cd419208668e8c840525322b72385732b4e
NeveenBeiram <neveenbeiram@yahoo.com>
2021-05-17 20:20:46 +0300

    done

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
-# basic-express-server
+401 class 02 lab
+# LAB - 02
+## basic-express-server
+### Author: Neveen Beiram
 
 # lab 02 
+
+
+* [deployment for main branch ](https://neveen-basic-express-server.herokuapp.com/) .
+* [submission PR](https://github.com/NeveenBeiram/basic-express-server/pulls) .
+* [tests report](https://github.com/NeveenBeiram/basic-express-server/actions) .
+ 
+### Setup
+
+#### `.env` requirements
+
+- `PORT` - 3000
+
+#### Running the app
+
+- `npm start`
+- Endpoint: `/`
+  - Returns message
+
+    ```
+
+    'welcome to server.js'
+
+    ```
+
+    - Endpoint: `/bad`
+  - Returns message
+
+    ```
+
+    'Something went wrong !!!!!';
+
+    ```
+
+    -Endpoint:`/person`
+    -Returns an object
+    
+    ```
+{
+    name:the name from the query
+}
+    ```
+
+     - Endpoint: **anything else ..**
+  - Returns an error 404
+  - Returns an Object
+
+    ```
+
+    {
+        "status": 404,
+        "message": "Sorry , Page not Found"
+    }
+
+    ```
+#### Tests
+
+- Unit Tests: `npm run test`
+- Lint Tests: `npm run lint`
+
--- src/server.js
+++ src/server.js
@@ -1,84 +1,20 @@
-// 'use strict';
-
-// const express = require('express');
-// const app = express();
-// const notFoundHandler = require('./error-handlers/404');
-// const errorHandler = require('./error-handlers/500');
-// const logger = require('./middleware/logger');
-// const validator = require('./middleware/validator');
-
-// app.use(express.json());
-// app.use(logger);
-// app.use(validator);
-
-
-
-// //localhost:3000/person?name=neveen
-// // app.get('/person',(req,res)=>{
-// //   const output={
-// //     name: req.query.name,
-// //   };
-// //   res.json(output);
-// // });
-
-// app.get('/', (req, res) =>{
-//   res.send('welcome to server.js');
-// });
-
-// app.get( '/bad',(req, res)  =>{
-//   throw new Error('Something went wrong !');
-// });
-
-// app.get('/person', validator, (req, res) => {
-//   res.status(200).json({
-//     name: req.query.name,
-//   });
-// });
-
-
-// //localhost:3000/person/neveen
-// // app.get('person/:name',(req,res)=>{
-// //   const output={
-// //     name: req.params.name,
-// //   };
-// //   res.json(output);
-// // });
-
-
-
-
-
-
-
-
-// app.use('*', notFoundHandler);
-// app.use(errorHandler);
-
-
-// function start(port) {
-//   app.listen(port, () => {
-//     console.log(`The server is listen on port ${port}`);
-//   });
-// }
-
-// module.exports = {
-//   app: app,
-//   start: start,
-// };
-
-
 'use strict';
+
 const express = require('express');
 const app = express();
+
 const errorHandler = require('./error-handlers/500.js');
 const notFoundHandler = require('./error-handlers/404.js');
+
 const logger = require('./middleware/logger.js');
 const validator = require('./middleware/validator.js');
+
 app.use(express.json());
 app.use(logger);
 
 app.get('/', home);
 app.get('/bad', badReq);
+
 function home(req,res){
   res.send('welcome to server.js');
 }
